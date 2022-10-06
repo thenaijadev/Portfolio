3543732c5e34ff2c6bb51446476503fff22e710a
Okwor Chukwuemeka <thenaiadev@gmail.com>
2022-10-06 11:16:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Description
+
+This is my portfolio built in React.
+
+### [live preview](https://sweet-bubblegum-0e7e09.netlify.app/)
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+If you like this portfolio template don't forget give it a ⭐
---
+++ src/content_option.js
@@ -0,0 +1,191 @@
+const logotext = "thenaijadev";
+const meta = {
+  title: "thenaijadev",
+  description:
+    "I’m Okwor chukwuemeka Nnajiofor a Front-end developer,currently working in Lagos",
+};
+
+const introdata = {
+  title: "I’m Okwor Chukwuemeka Nnajiofor",
+  animated: {
+    first: "Front-end developer",
+    second: "I build websites.",
+    third: "I love react",
+  },
+  description:
+    "Front-end developer who loves react and building web and mobile applications with react and flutter.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "Who am I",
+  aboutme:
+    "I am a front end web and mobile developer with 1 year experience building applications with react and flutter.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Front-end developer",
+    where: "Nubiaville",
+    date: "September 2022 - Present",
+  },
+  {
+    jobtitle: "Emerging tech intern",
+    where: "KPMG",
+    date: "August 2022 - October 2022",
+  },
+  {
+    jobtitle: "Mobile developer intern (Flutter)",
+    where: "Side Hustle",
+    date: "March 2022 - May 2022",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 90,
+  },
+
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 100,
+  },
+  {
+    name: "Jquery",
+    value: 80,
+  },
+  {
+    name: "Flutter",
+    value: 90,
+  },
+];
+
+const services = [
+  {
+    title: "Front-end",
+    description:
+      "I build web applications using react and associated technologies.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Flutter is a beautiful SDK I love building applications with.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "thenaijadev@gmail.com",
+  YOUR_FONE: "+2349014138731",
+  description:
+    "Send me a message and I will be sure to apply as soon as I can ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/thenaijadev",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/okwor-chukwuemeka-01568b20b/",
+  twitter: "https://twitter.com/thenaijadev",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
